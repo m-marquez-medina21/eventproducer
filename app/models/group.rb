598981cdb508b22c62d0c7397a63ad57d77184d0
m-marquez-medina21 <m.marquez.medina21@gmail.com>
2021-08-09 00:46:35 -0400

    first commit

---
+++ app/models/group.rb
@@ -0,0 +1,15 @@
+class Group < ApplicationRecord
+    has_many  :concerts, dependent: :destroy
+    enum gender: { BoyBand: 0, GirlBand: 1, Band: 3 }
+    accepts_nested_attributes_for :concerts
+    def to_s
+        "#{name}"
+    end
+    def concert_count
+        self.concerts.count
+    end
+    def concert_participant
+        self.concerts
+    end
+
+end
